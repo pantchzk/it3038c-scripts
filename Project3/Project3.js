3c4f8176323ca90996b78348b0b08254492fa64e
pantchzk <122412557+pantchzk@users.noreply.github.com>
2023-04-23 20:29:26 -0400

    Update Project3.js

--- Project3/Project3.js
+++ Project3/Project3.js
@@ -2,11 +2,11 @@ var http = require('http')
 var fs = require('fs')
 
 
-
-const prompt = require("prompt-sync")({ sigint: true });
-var img = prompt("\nEnter a URL for a JPG or PNG (or leave blank for an example image): ");
 const ColorThief = require('colorthief');
+const prompt = require("prompt-sync")({ sigint: true });
 
+// uses prompt-sync package to allow user input for var img
+var img = prompt("\nEnter a URL for a JPG or PNG (or leave blank for an example image): ");
 
 //Sample photograph I took that can be used as image if user input is left blank
 if (img == "")
@@ -14,10 +14,12 @@ if (img == "")
     img = "https://raw.githubusercontent.com/pantchzk/it3038c-scripts/main/Project3/Forest.jpg"
 }
 
+// uses colorthief package to get RGB value of image based on img var source
 ColorThief.getColor(img)
     .then(color => { console.log("\nThe RGB value for the primary color of this image is: " + color) })
     .catch(err => { console.log(err) })
 
+//creates a local web-server on port 3000 that contains the image to view and URL to image
     http.createServer((req, res) => {
         if (req.url === "/") {
         fs.readFile("index.html", "UTF-8", (err, body) => {
@@ -39,7 +41,3 @@ ColorThief.getColor(img)
     }}).listen(3000)
 
 console.log("\nYou can view the image at http://localhost:3000/")
-
-    //https://stackoverflow.com/questions/61394928/get-user-input-through-node-js-console
-    //https://www.npmjs.com/package/prompt-sync
-    //https://lokeshdhakar.com/projects/color-thief/
